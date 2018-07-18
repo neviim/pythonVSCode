c149d026731e7d69d3f195d9b031f251ffa278d2
Don Jayamanne <don.jayamanne@yahoo.com>
2018-07-18 16:35:34 -0700

    Move service registrations from debug adapter host to extension host

--- src/client/activation/serviceRegistry.ts
+++ src/client/activation/serviceRegistry.ts
@@ -8,9 +8,14 @@ import { ExtensionActivationService } from './activationService';
 import { JediExtensionActivator } from './jedi';
 import { LanguageServerExtensionActivator } from './languageServer';
 import { ExtensionActivators, IExtensionActivationService, IExtensionActivator } from './types';
+import { IPythonExtensionBanner, BANNER_NAME_LS_SURVEY, BANNER_NAME_PROPOSE_LS } from '../common/types';
+import { LanguageServerSurveyBanner } from '../languageServices/languageServerSurveyBanner';
+import { ProposeLanguageServerBanner } from '../languageServices/proposeLanguageServerBanner';
 
 export function registerTypes(serviceManager: IServiceManager) {
     serviceManager.addSingleton<IExtensionActivationService>(IExtensionActivationService, ExtensionActivationService);
     serviceManager.add<IExtensionActivator>(IExtensionActivator, JediExtensionActivator, ExtensionActivators.Jedi);
     serviceManager.add<IExtensionActivator>(IExtensionActivator, LanguageServerExtensionActivator, ExtensionActivators.DotNet);
+    serviceManager.addSingleton<IPythonExtensionBanner>(IPythonExtensionBanner, LanguageServerSurveyBanner, BANNER_NAME_LS_SURVEY);
+    serviceManager.addSingleton<IPythonExtensionBanner>(IPythonExtensionBanner, ProposeLanguageServerBanner, BANNER_NAME_PROPOSE_LS);
 }
